e6842b7a9741a78817f7adf7b82ec6377e59589d
Vijay9686 <105700592+Vijay9686@users.noreply.github.com>
2022-06-13 20:12:46 +0530

    Add files via upload

---
+++ java2.java
@@ -0,0 +1,28 @@
+
+public class java2 {
+
+	public static void main(String[] args) 
+	{
+		int s=7;
+		  if(s<70)
+		  {
+			  if(s%2==0)
+				  System.out.println("s is an even number and greaterthan 10!");
+			  else
+				  System.out.println("s is a odd number and greater than 10!");
+		  }
+		  else
+		  {
+			  System.out.println("s is less than 10");
+			  
+		  }
+		  System.out.println("Hello World!");
+		  
+
+		}
+
+	}
+
+	
+
+
